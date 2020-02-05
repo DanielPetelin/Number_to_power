e9558b3b9ccdaf302526bd8f8f7f0b09799d769a
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-02-05 14:54:05 +0300

    add step to 10

--- js/script.js
+++ js/script.js
@@ -1,20 +1,62 @@
 document.getElementById('step2').onclick = function() {
     var num = document.getElementById('num').value;
-    var result = num * num;
+    var result = Math.pow(num, 2);
 
     document.getElementById('out').innerHTML = result;
 };
 
 document.getElementById('step3').onclick = function() {
     var num = document.getElementById('num').value;
-    var result = num * num * num;
+    var result = Math.pow(num, 3);
 
     document.getElementById('out').innerHTML = result;
 };
 
 document.getElementById('step4').onclick = function() {
     var num = document.getElementById('num').value;
-    var result = num * num * num * num;
+    var result = Math.pow(num, 4);
+
+    document.getElementById('out').innerHTML = result;
+};
+
+document.getElementById('step5').onclick = function() {
+    var num = document.getElementById('num').value;
+    var result = Math.pow(num, 5);
+
+    document.getElementById('out').innerHTML = result;
+};
+
+document.getElementById('step6').onclick = function() {
+    var num = document.getElementById('num').value;
+    var result = Math.pow(num, 6);
+
+    document.getElementById('out').innerHTML = result;
+};
+
+document.getElementById('step7').onclick = function() {
+    var num = document.getElementById('num').value;
+    var result = Math.pow(num, 7);
+
+    document.getElementById('out').innerHTML = result;
+};
+
+document.getElementById('step8').onclick = function() {
+    var num = document.getElementById('num').value;
+    var result = Math.pow(num, 8);
+
+    document.getElementById('out').innerHTML = result;
+};
+
+document.getElementById('step9').onclick = function() {
+    var num = document.getElementById('num').value;
+    var result = Math.pow(num, 9);
+
+    document.getElementById('out').innerHTML = result;
+};
+
+document.getElementById('step10').onclick = function() {
+    var num = document.getElementById('num').value;
+    var result = Math.pow(num, 10);
 
     document.getElementById('out').innerHTML = result;
 };
